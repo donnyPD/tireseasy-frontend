216358c9cdac4309890fdc704896a2711fc6c2cc
aduda <duda.a@kozak-group.com>
2025-10-10 16:09:23 +0200

    TE1-T108 - added changes as per requested FE adjustment

--- src/components/OrderHistoryPage/OrderDetails/orderDetails.js
+++ src/components/OrderHistoryPage/OrderDetails/orderDetails.js
@@ -52,18 +52,18 @@ const OrderDetails = props => {
                     <ShippingMethod data={shippingMethodData} />
                 </ConditionalWrapper>
             </div>
-            <div className={classes.billingInformationContainer}>
-                <ConditionalWrapper condition={billing_address}>
-                    <BillingInformation data={billing_address} />
-                </ConditionalWrapper>
-            </div>
-            <div className={classes.paymentMethodContainer}>
-                <ConditionalWrapper
-                    condition={payment_methods && payment_methods.length}
-                >
-                    <PaymentMethod data={payment_methods} />
-                </ConditionalWrapper>
-            </div>
+            {/*<div className={classes.billingInformationContainer}>*/}
+            {/*    <ConditionalWrapper condition={billing_address}>*/}
+            {/*        <BillingInformation data={billing_address} />*/}
+            {/*    </ConditionalWrapper>*/}
+            {/*</div>*/}
+            {/*<div className={classes.paymentMethodContainer}>*/}
+            {/*    <ConditionalWrapper*/}
+            {/*        condition={payment_methods && payment_methods.length}*/}
+            {/*    >*/}
+            {/*        <PaymentMethod data={payment_methods} />*/}
+            {/*    </ConditionalWrapper>*/}
+            {/*</div>*/}
             <div className={classes.itemsContainer}>
                 <ConditionalWrapper condition={items && items.length}>
                     <Items data={{ imagesData: imagesArray, items }} />
