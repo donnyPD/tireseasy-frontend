527bdb81e2d4880d94f7fdfe0d78d27f92ac1bd4
artem.d@pineappledevelopment.com <duda.a@kozak-group.com>
2025-10-28 14:49:25 +0100

    TE1-T97 - Staggered search - added new parameters from url to filter

---
+++ src/talons/RootComponents/Category/helpers.js
@@ -0,0 +1,38 @@
+/**
+ * Looks for filter values within a search string and returns a map like
+ * {
+ *   "category_id": ["Bottoms,28", "Pants & Shorts,19"]
+ * }
+ * filter[category_id]=Bottoms,28&filter[category_id]=Pants & Shorts,19
+ * @param {String} initialValue a search string, as in from location.search
+ */
+export const getCustomFiltersFromSearch = initialValue => {
+    // preserve all existing params
+    const params = new URLSearchParams(initialValue);
+    const uniqueKeys = new Set(params.keys());
+    const filters = new Map();
+
+    // iterate over existing param keys
+    for (const key of uniqueKeys) {
+        // if a key matches a known filter, add its items to the next state
+        if (key.startsWith('front_') || key.startsWith('rear_')) {
+            // derive the group by slicing off `[filter]`
+            const group = key;
+            const items = new Set();
+
+            // map item values to items
+            for (const value of params.getAll(key)) {
+                items.add(value);
+            }
+
+            // add items to the next state, keyed by group
+            filters.set(group, items);
+        }
+    }
+
+    return filters;
+};
+
+export const getCustomFilterInput = (value) => {
+    return { eq: value.values().next().value};
+};
